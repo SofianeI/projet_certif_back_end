2c4693ae5192c07149c58243596a3905ff5facb3
sofiane <idri.sof3@gmail.com>
2023-11-30 09:28:47 -0500

    creation de projet spring boot

---
+++ src/main/java/com/projetCertif/ProjetCertifApplication.java
@@ -0,0 +1,13 @@
+package com.projetCertif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCertifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCertifApplication.class, args);
+	}
+
+}
