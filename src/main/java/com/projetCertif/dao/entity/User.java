7500458ba88a78f3cb3616f1884d0cf58411cd53
Alexandre BOUIRIK <alexandre.bouirik@outlook.com>
2023-11-30 19:08:31 +0100

    - modif entity with lombok - repository 3 entity - DbAccessTests all tests ok

---
+++ src/main/java/com/projetCertif/dao/entity/User.java
@@ -0,0 +1,24 @@
+package com.projetCertif.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "picture")
+    private String picture;
+
+
+}
