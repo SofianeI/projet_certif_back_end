84079696f1ac34fe19db05d18e6d41b9d0653337
sofiane <idri.sof3@gmail.com>
2023-12-01 11:18:34 -0500

    add function entity DTO

--- src/main/java/com/projetCertif/dao/entityDto/UserDto.java
+++ src/main/java/com/projetCertif/dao/entityDto/UserDto.java
@@ -1,5 +1,6 @@
 package com.projetCertif.dao.entityDto;
 
+import com.projetCertif.dao.entity.User;
 import lombok.*;
 
 @Data
@@ -10,6 +11,34 @@ public class UserDto {
     private String prenom;
     private String nom;
     private String image;
+
+        public static  UserDto fromEntity(User user) {
+        if (user == null){
+            //TODO throw an exception
+            return null;
+        }
+        return UserDto.builder()
+                .id(user.getId())
+                .prenom(user.getFirstname())
+                .nom(user.getLastname())
+                .image(user.getPicture())
+                .build();
+    }
+
+    public static User toEntity(UserDto userDto){
+        if(userDto == null){
+            //TODO throw an exception
+            return  null;
+        }
+        User user= new User();
+        user.setId(userDto.getId());
+        user.setFirstname(userDto.getPrenom());
+        user.setLastname(userDto.getNom());
+        user.setPicture(userDto.getImage());
+        return user;
+
+    }
+
 
 
 }
